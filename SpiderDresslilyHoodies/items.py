6b89926abae45cabd4351fb2655111aab39e4849
YaroslavChyhryn <85682486+YaroslavChyhryn@users.noreply.github.com>
2021-07-12 21:23:26 +0300

    fix: discount and discount price

--- SpiderDresslilyHoodies/items.py
+++ SpiderDresslilyHoodies/items.py
@@ -1,6 +1,5 @@
 import scrapy
-from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose, Join
+from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
 import re
 from datetime import datetime
@@ -17,10 +16,23 @@ def format_product_info(product_info):
     return product_info
 
 
-def price_to_int(price):
-    # todo плохая идея хранить деньги во float
-    return float(price)
+def string_to_int(price):
+    try:
+        return int(price)
+    except ValueError:
+        return []
 
+def string_to_float(price):
+    try:
+        return float(price)
+    except ValueError:
+        return []
+
+def check_display_attr(element):
+    if re.search(r'inline-block', element):
+        return re.findall(r'data-orgp="(.*?)"', element)
+    else:
+        return []
 
 def url_from_splash_url(url):
     return re.findall(r'url=(.*?)(&time)', url)
@@ -45,28 +57,42 @@ def format_size(size):
         return []
 
 def format_time(time):
-    # todo проверить правильность перевода в unix timestamp
     datetime_obj = datetime.strptime(time, '%b,%d %Y %H:%M:%S')
     return datetime_obj.timestamp()
 
 
 class SpiderdresslilyHoodiesItem(scrapy.Item):
-    product_id = scrapy.Field(input_processor=(MapCompose(product_id_from_product_url)), output_processor=TakeFirst())
+    product_id = scrapy.Field(input_processor=(MapCompose(product_id_from_product_url)),
+                              output_processor=TakeFirst())
     product_url = scrapy.Field(output_processor=TakeFirst())
-    name = scrapy.Field(input_processor=(MapCompose(remove_tags)), output_processor=TakeFirst())
-    discount = scrapy.Field(input_processor=(MapCompose(remove_tags)), output_processor=TakeFirst())
-    discounted_price = scrapy.Field(input_processor=(MapCompose(price_to_int)), output_processor=TakeFirst())
-    original_price = scrapy.Field(input_processor=(MapCompose(price_to_int)), output_processor=TakeFirst())
-    total_reviews = scrapy.Field(input_processor=(MapCompose(remove_tags)), output_processor=TakeFirst())
-    product_info = scrapy.Field(input_processor=(MapCompose(format_product_info)), output_processor=TakeFirst())
+    name = scrapy.Field(input_processor=(MapCompose(remove_tags)),
+                        output_processor=TakeFirst())
+    discount = scrapy.Field(input_processor=(MapCompose(remove_tags,
+                                                        string_to_int)),
+                            output_processor=TakeFirst())
+    discounted_price = scrapy.Field(input_processor=(MapCompose(check_display_attr,
+                                                                string_to_float)),
+                                    output_processor=TakeFirst())
+    original_price = scrapy.Field(input_processor=(MapCompose(string_to_float)),
+                                  output_processor=TakeFirst())
+    total_reviews = scrapy.Field(input_processor=(MapCompose(remove_tags,
+                                                             string_to_int)),
+                                 output_processor=TakeFirst())
+    product_info = scrapy.Field(input_processor=(MapCompose(format_product_info)),
+                                output_processor=TakeFirst())
 
 
 class SpiderdresslilyReviewsItem(scrapy.Item):
-    product_id = scrapy.Field(input_processor=(MapCompose(product_id_from_product_url)), output_processor=TakeFirst())
-    rating = scrapy.Field(input_processor=(MapCompose(count_rating)), output_processor=TakeFirst())
-    timestamp = scrapy.Field(input_processor=(MapCompose(remove_tags, format_time)), output_processor=TakeFirst())
-    # todo Проверить кодировку, не все отображается корректно в excel
-    text = scrapy.Field(input_processor=(MapCompose(remove_tags)), output_processor=TakeFirst())
-    size = scrapy.Field(input_processor=(MapCompose(format_size)), output_processor=TakeFirst())
-    color = scrapy.Field(input_processor=(MapCompose(format_color)), output_processor=TakeFirst())
+    product_id = scrapy.Field(input_processor=(MapCompose(product_id_from_product_url)),
+                              output_processor=TakeFirst())
+    rating = scrapy.Field(input_processor=(MapCompose(count_rating)),
+                          output_processor=TakeFirst())
+    timestamp = scrapy.Field(input_processor=(MapCompose(remove_tags, format_time)),
+                             output_processor=TakeFirst())
+    text = scrapy.Field(input_processor=(MapCompose(remove_tags)),
+                        output_processor=TakeFirst())
+    size = scrapy.Field(input_processor=(MapCompose(format_size)),
+                        output_processor=TakeFirst())
+    color = scrapy.Field(input_processor=(MapCompose(format_color)),
+                         output_processor=TakeFirst())
 
--- SpiderDresslilyHoodies/spiders/hoodies.py
+++ SpiderDresslilyHoodies/spiders/hoodies.py
@@ -1,5 +1,4 @@
 import scrapy
-import re
 from scrapy_splash import SplashRequest
 from SpiderDresslilyHoodies.items import SpiderdresslilyHoodiesItem
 from scrapy.loader import ItemLoader
@@ -15,7 +14,7 @@ class HoodieSpider(scrapy.Spider):
         hoodie_page_links = response.css('a.goods-name-link::attr(href)').getall()
 
         for url in hoodie_page_links:
-            yield SplashRequest(url, self.parse_hoodie, args={'wait': 1})
+            yield SplashRequest(url, self.parse_hoodie, args={'timeout': 10, 'wait': 5})
 
         next_page_partial_url = response.css('li.next-page a').attrib['href']
         if next_page_partial_url is not None:
@@ -26,15 +25,11 @@ class HoodieSpider(scrapy.Spider):
         l = ItemLoader(item=SpiderdresslilyHoodiesItem(), response=response)
 
         l.add_value('product_id', response.url)
-        # todo Поискать ID в исходниках, не уверен что это он
         l.add_value('product_url', response.url)
         l.add_css('name', 'span.goodtitle')
-        l.add_xpath('discount', '//span[@class="off js-dl-cutoff"]/span/text()')    #add splash
-        discounted_price = re.findall('o.goods.price = "(.+?)",\n', response.body.decode("utf-8"), re.S)[0]
-        l.add_value('discounted_price', discounted_price)
-        original_price = re.findall('o.goods.market_price = "(.+?)",\n', response.body.decode("utf-8"), re.S)[0]
-        l.add_value('original_price', original_price)
-        # l.add_xpath('total_reviews', '//div[contains(@class, "slidetitle")]/strong/')
+        l.add_xpath('discount', '//span[@class="off js-dl-cutoff"]/span')
+        l.add_css('original_price', 'span.curPrice::attr(data-orgp)')
+        l.add_css('discounted_price', 'span.js-dl-marketPrice')
         l.add_css('total_reviews', 'strong#js_reviewCountText')
 
         l.add_xpath('product_info', '//div[@class="xxkkk20"]')
